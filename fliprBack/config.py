58136d8531e411dd163636fc59f2e2877dc871b9
Harendra8095 <harendrarathor444@gmail.com>
2021-05-11 13:35:41 +0530

    change database name to flipr

--- fliprBack/config.py
+++ fliprBack/config.py
@@ -17,9 +17,9 @@ class DbEngine_config():
     DB_PORT = os.environ.get('DB_PORT') or '5432'
     DB_USER = os.environ.get('DB_USER') or 'postgres'
     DB_PASS = os.environ.get('DB_PASS') or 'postgres'
-    DB_NAME = os.environ.get('DB_NAME') or 'flipr_test'
+    DB_NAME = os.environ.get('DB_NAME') or 'flipr'
     if(os.environ.get('TEST') == '1'):
-        DB_NAME = 'flipr_test'
+        DB_NAME = 'flipr'
     print(DB_NAME)
     DB_URL = f'{DB_DIALECT}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     print(DB_URL)
